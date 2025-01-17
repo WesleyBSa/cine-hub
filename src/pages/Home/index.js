b7246f96a7f377853aca4344c1dbfe66b1ae90f1
WesleyBSa <batistawesley948@gmail.com>
2025-01-17 15:55:53 -0300

    Initial commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,28 +3,35 @@ import api from '../../services/api';
 import { Link } from 'react-router-dom';
 import './home.css';
 
-//URL da API: https://api.themoviedb.org/3/movie/now_playing?api_key=bbdc9ce8fa2594ce47106774beb04d7b&language=pt-BR
-
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilmes() {
             const response = await api.get("movie/now_playing", {
                 params: {
-                    api_key:"bbdc9ce8fa2594ce47106774beb04d7b",
+                    api_key: process.env.REACT_APP_API_KEY,
                     language: "pt-BR",
                     page: 1,
                 }
             })
 
-            //console.log(response.data.results.slice(0, 10));
-            setFilmes(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 20));
+            setLoading(false);
         }
 
         loadFilmes();
     }, [])
+
+    if(loading){
+        return(
+           <div className="loading">
+            <h2>Carregando filmes...</h2>
+           </div> 
+        )
+    }
 
     return(
         <div className="container">
